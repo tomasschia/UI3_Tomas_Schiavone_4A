d314d754cdc015a27a91cba40aff5b53e53b444a
tomasschia <148562630+tomasschia@users.noreply.github.com>
2024-05-27 14:13:41 -0300

    EAC REALIZADO
    se hizo una comparacion entre un imput y una cotraseña preestablecida mediante una variable publica

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class LoginManager : MonoBehaviour
+{
+    public string contraseña_god;
+    public InputField contra; 
+    
+  
+      public void CHEKEARCONTRASEÑA()
+      {
+         if (contraseña_god == contra.text)
+         {
+            Debug.Log("acces granted");
+         }
+         else 
+         {
+            Debug.Log("acces denied"); 
+         }
+      }
+
+}
